ff604c133f6a2967690c48d230c7e65d50e3c8cd
Artsiom Tsvikevich <temancvik@gmail.com>
2021-01-29 23:19:38 +0300

    second

--- src/test/java/By_Variables.java
+++ src/test/java/By_Variables.java
@@ -1,9 +1,25 @@
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 
 public class By_Variables {
     @Test
     public void location() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.tut.by/");
 
+        WebElement icon = driver.findElement(By.className("header-search"));
+        WebElement icon2 = driver.findElement(By.cssSelector("#mainmenu > ul > li:nth-child(3) > a"));
+        WebElement icon3 = driver.findElement(By.xpath("//*[@id=\"geo_news_block\"]/div/div[1]/form/div[1]/a"));
+        WebElement icon4 = driver.findElement(By.id("animated_mainmenu"));
+        WebElement icon5 = driver.findElement(By.tagName("a"));
+        WebElement icon6 = driver.findElement(By.name("lsgetframe"));
+        WebElement icon7 = driver.findElement(By.linkText("Каталог цен"));   // this i'm not sure
+        WebElement icon8 = driver.findElement(By.partialLinkText("Каталог"));  // this i'm not sure
 
+        driver.quit();
     }
 }
--- src/test/java/Task30.java
+++ src/test/java/Task30.java
@@ -1,8 +1,32 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 
 public class Task30 {
     @Test
     public void login() {
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.tut.by/");
+        driver.findElement(By.cssSelector("#authorize > div > a")).click();
+
+        WebElement username = driver.findElement(By.name("login"));
+        WebElement password = driver.findElement(By.name("password"));
+        WebElement login = driver.findElement(By.cssSelector("#authorize > div > div > div > div.b-auth-form__inner > form > div:nth-child(3)"));
+
+        username.sendKeys("seleniumtests@tut.by");
+        password.sendKeys("123456789zxcvbn");
+        login.click();
+
+        String result = driver.findElement(By.cssSelector("#authorize > div.b-auth-i > a > span.uname")).getText();
+        String expectedname = "Selenium Test";
+        Assert.assertEquals(expectedname, result);
+
+        driver.quit();
 
     }
 
